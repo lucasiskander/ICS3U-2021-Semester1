d68c81039d9a6cf55d8380f8d57bb2df41c2b2dc
Lucas Iskander <liskander@bayviewglen.ca>
2021-09-09 09:36:39 -0400

    change

---
+++ Unit One - Introduction to Cs/src/Week1/ExampleOne.java
@@ -0,0 +1,12 @@
+package Week1;
+
+//Every file will be its own class FOR NOW
+public class ExampleOne {
+    public static void main(String[] args) {
+        //The main method (function) lets java know here to start
+        System.out.println("Welcome to ICS 3u - AP");
+        System.out.println("Welcome to ICS 3u - AP");
+        System.out.println("Welcome to ICS 3u - AP");
+        //System.out.printIn writes to the console. Whatever is in the round bracket goes in the console
+    }
+}
---
+++ Unit One - Introduction to Cs/src/Week1/ExampleTwo.java
@@ -0,0 +1,11 @@
+package Week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.print("B");
+        System.out.print("C");
+        System.out.println("D");
+        System.out.println("E");
+    }
+}
